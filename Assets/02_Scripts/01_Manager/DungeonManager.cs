7da5d5a1499924cba3a9cec0e22f42d5d75f394b
Natai98 <mrscol0520@naver.com>
2025-11-21 13:04:19 +0900

    Change DungeonManager

--- Assets/02_Scripts/01_Manager/DungeonManager.cs
+++ Assets/02_Scripts/01_Manager/DungeonManager.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class DungeonManager : Singleton<DungeonManager>
 {
+    [Header("전투 패턴 관련")]
     public bool fireDogGrog = false;
+    public bool bossGrog = false;
+    public bool inShield = false;
+    public bool bossTrigger = false;
+
+    [Header("던전 기믹 관련")]
     public bool pannalClear = false;
 
     public void ChangeMaterial(Renderer ren, Material mat, int index)
