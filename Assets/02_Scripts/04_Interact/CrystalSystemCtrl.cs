40702a6fb12d14109dfbfabec18783841022f202
Natai98 <mrscol0520@naver.com>
2025-12-16 19:30:11 +0900

    Dungeon End Prod

--- Assets/02_Scripts/04_Interact/CrystalSystemCtrl.cs
+++ Assets/02_Scripts/04_Interact/CrystalSystemCtrl.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CrystalSystemCtrl : MonoBehaviour
 {
     [SerializeField] private Transform Room8;
+    private PlayableDirector endProd;
     private void Start()
     {
+        endProd = GetComponent<PlayableDirector>();
         StartCoroutine(CrySystem());
     }
 
@@ -20,7 +23,14 @@ public class CrystalSystemCtrl : MonoBehaviour
         Room8.gameObject.SetActive(true);
         yield return new WaitUntil(() => GameManager.Instance.crystal >= 3);
         transform.GetChild(2).gameObject.SetActive(true);
+        yield return new WaitUntil(() => Vector3.Distance(GameManager.Instance.player.transform.position, transform.position) < 3.0f);
+        GameManager.Instance.player.GetComponent<PlayerMove>().isMove = false;
+        endProd.Play();
         yield return null;
+        yield return new WaitForSeconds((float)endProd.duration);
+        yield return new WaitForSeconds(0.3f);
+        GameManager.Instance.player.GetComponent<PlayerMove>().isMove = true;
+
     }
 
     private void OnDisable()
